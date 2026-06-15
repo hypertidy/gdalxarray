ec7f22216157402f9e4896f121d74515ab5954ad
Michael Sumner <mdsumner@gmail.com>
2026-06-15 23:26:16 +0000

    prepare version bump

--- gdalxarray/__init__.py
+++ gdalxarray/__init__.py
@@ -16,7 +16,7 @@ except ImportError as e:
 
 # Single source of truth for the version string. Hatchling parses this line
 # at build time via [tool.hatch.version] path = "gdalxarray/__init__.py".
-__version__ = "0.3.0.9001"
+__version__ = "0.3.1"
 
 from .backend import GDALBackendEntrypoint
 
